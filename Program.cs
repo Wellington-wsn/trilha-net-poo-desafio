53e756197dd60fc1c12eac38438b47bf696d38f0
Wellington-wsn <fogosark@hotmail.com>
2023-01-27 21:19:50 -0300

    Finalizado desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1844456", modelo: "Tejolo", imei: "111111111", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("\n");
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "47587", modelo: "ZOOM-Out", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
